64fcfe8c7e35ae42b618e8cb9067bc1c13bb2df5
Pratik Singh <122256468+PratikSingh30@users.noreply.github.com>
2023-07-26 23:00:54 +0530

    Add files via upload

---
+++ BirthDay_Wish.py
@@ -0,0 +1,10 @@
+
+def happy_birthday(friend_name):
+  """Wishes a happy birthday to the specified friend."""
+  print(f"Happy birthday to my dear friend {friend_name}!")
+  print(f"I hope you have a wonderful day filled with love, laughter, and cake.")
+  print(f"May all your dreams come true this year.")
+
+if __name__ == "__main__":
+  friend_name = input("Enter your name: ")
+  happy_birthday(friend_name)
